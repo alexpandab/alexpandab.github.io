12a816965810e923bd99ff7d940e73bf064ee731
alexpandab <alex.baird@mtchs.org>
2016-12-14 10:57:42 -0700

    Add files via upload

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,32 @@
-function selectItem(){
-	
+var selectedElement = "";
+function documentLoad(){
+	selectedElement = document.getElementById('works');
+}
+
+function selectItem(tab){
+	console.log(selectedElement);
+	selectedElement.className = "";
+	selectedElement = document.getElementById(tab);
+	selectedElement.className = "selectedTab";
+	switch(tab){
+		case('works'):
+
+		break;
+		case('s1'):
+
+		break;
+		case('s2'):
+
+		break;
+		case('s3'):
+
+		break;
+		case('s4'):
+
+		break;
+		case('s5'):
+
+		break;
+	}
+	console.log(document.getElementById(tab).className);
 }
