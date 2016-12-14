45f9318f1af3d055391669ce0108da5fe22e6927
alexpandab <alex.baird@mtchs.org>
2016-12-14 15:20:59 -0700

    Add files via upload

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,29 +4,57 @@ function documentLoad(){
 }
 
 function selectItem(tab){
-	console.log(selectedElement);
 	selectedElement.className = "";
 	selectedElement = document.getElementById(tab);
 	selectedElement.className = "selectedTab";
 	switch(tab){
 		case('works'):
-			
+			$('#worksBody').animate({marginLeft: "0"});
+			$('#s1Body').animate({marginLeft: "100%"});
+			$('#s2Body').animate({marginLeft: "100%"});
+			$('#s3Body').animate({marginLeft: "100%"});
+			$('#s4Body').animate({marginLeft: "100%"});
+			$('#s5Body').animate({marginLeft: "100%"});
 		break;
 		case('s1'):
-
+			$('#worksBody').animate({marginLeft: "-100%"});
+			$('#s1Body').animate({marginLeft: "0%"});
+			$('#s2Body').animate({marginLeft: "100%"});
+			$('#s3Body').animate({marginLeft: "100%"});
+			$('#s4Body').animate({marginLeft: "100%"});
+			$('#s5Body').animate({marginLeft: "100%"});
 		break;
 		case('s2'):
-
+			$('#worksBody').animate({marginLeft: "-100%"});
+			$('#s1Body').animate({marginLeft: "-100%"});
+			$('#s2Body').animate({marginLeft: "0%"});
+			$('#s3Body').animate({marginLeft: "100%"});
+			$('#s4Body').animate({marginLeft: "100%"});
+			$('#s5Body').animate({marginLeft: "100%"});
 		break;
 		case('s3'):
-
+			$('#worksBody').animate({marginLeft: "-100%"});
+			$('#s1Body').animate({marginLeft: "-100%"});
+			$('#s2Body').animate({marginLeft: "-100%"});
+			$('#s3Body').animate({marginLeft: "0%"});
+			$('#s4Body').animate({marginLeft: "100%"});
+			$('#s5Body').animate({marginLeft: "100%"});
 		break;
 		case('s4'):
-
+			$('#worksBody').animate({marginLeft: "-100%"});
+			$('#s1Body').animate({marginLeft: "-100%"});
+			$('#s2Body').animate({marginLeft: "-100%"});
+			$('#s3Body').animate({marginLeft: "-100%"});
+			$('#s4Body').animate({marginLeft: "0%"});
+			$('#s5Body').animate({marginLeft: "100%"});
 		break;
 		case('s5'):
-
+			$('#worksBody').animate({marginLeft: "-100%"});
+			$('#s1Body').animate({marginLeft: "-100%"});
+			$('#s2Body').animate({marginLeft: "-100%"});
+			$('#s3Body').animate({marginLeft: "-100%"});
+			$('#s4Body').animate({marginLeft: "-100%"});
+			$('#s5Body').animate({marginLeft: "0%"});
 		break;
 	}
-	console.log(document.getElementById(tab).className);
 }
